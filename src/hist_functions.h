6e198dcb324abb7d873ae188af34054fcca62f01
tverstraaten <twverstraaten@gmail.com>
2026-06-15 13:44:41 +0200

    improve benchmakrs

--- src/hist_functions.h
+++ src/hist_functions.h
@@ -12,104 +12,97 @@
 #include <string_view>
 
 static constexpr size_t SAMPLES = 5'000'000;
-static constexpr size_t SEED    = 12344321;
+static constexpr size_t SEED    = 12345654321;
 static volatile int     sink;
 static const Corpus     g_corpus = load_corpus("text_2.txt");
 
-static constexpr auto EMPTY_STR = "";
+static constexpr auto EMPTY_STR = "__EMPTY__";
 static constexpr auto EMPTY_SV  = std::string_view{EMPTY_STR};
 
-inline Histogram hist_find_raw_uint32_t(size_t n) {
+inline std::vector<uint32_t> random_vec(size_t n) {
     std::mt19937 rng(SEED);
-    Histogram    stats;
-    size_t       its = 0;
 
-    while (its < SAMPLES) {
-        RawHashMap<uint32_t, size_t> map;
-        {
-            for (size_t i = 0; i < n; i++) {
-                map[rng()] += 1;
-            }
-        }
-        for (size_t r = 0; r < n && its < SAMPLES; r++) {
-            ++its;
+    std::vector<uint32_t> rngs;
+    rngs.reserve(n);
+    for (size_t i = 0; i != SAMPLES; ++i) {
+        rngs.push_back(rng());
+    }
+    return rngs;
+}
 
-            asm volatile("" ::: "memory");
-            uint64_t t0 = rdtsc_start();
+inline Histogram hist_find_raw_uint32_t(size_t n) {
+    auto rngs = random_vec(n);
 
-            auto* p = map.get(rng());
+    Histogram                    stats;
+    RawHashMap<uint32_t, size_t> map;
+    for (size_t i = 0; i < n; i++) {
+        map[rngs[i]] += 1;
+    }
+    for (size_t r = 0; r < SAMPLES; r++) {
+        auto num = rngs[r % n];
+        asm volatile("" ::: "memory");
+        uint64_t t0 = rdtsc_start();
 
-            asm volatile("" ::: "memory");
-            doNotOptimize(p);
-            if (p) {
-                size_t v = *p;
-                sink += v;
-            }
-            uint64_t t1 = rdtsc_end();
-            stats.observe((double)(t1 - t0));
-        }
+        auto* p = map.get(num);
+
+        asm volatile("" ::: "memory");
+        doNotOptimize(p);
+
+        uint64_t t1 = rdtsc_end();
+        sink += *p;
+        stats.observe((double)(t1 - t0));
     }
     return stats;
 }
 
 inline Histogram hist_find_google_uint32_t(size_t n) {
-    std::mt19937 rng(SEED);
-    Histogram    stats;
-    size_t       its = 0;
+    auto rngs = random_vec(n);
 
-    while (its < SAMPLES) {
-        google::dense_hash_map<uint32_t, size_t> map;
-        map.set_empty_key(std::numeric_limits<uint32_t>::max());
-        {
-            for (size_t i = 0; i < n; i++) {
-                map[rng()] += 1;
-            }
-        }
-        for (size_t r = 0; r < n && its < SAMPLES; r++) {
-            ++its;
+    Histogram                                stats;
+    google::dense_hash_map<uint32_t, size_t> map;
+    map.set_empty_key(std::numeric_limits<uint32_t>::max());
+    for (size_t i = 0; i < n; i++) {
+        map[rngs[i]] += 1;
+    }
+    for (size_t r = 0; r < SAMPLES; r++) {
+        auto num = rngs[r % n];
+        asm volatile("" ::: "memory");
+        uint64_t t0 = rdtsc_start();
 
-            asm volatile("" ::: "memory");
-            uint64_t t0 = rdtsc_start();
+        auto p = map.find(num);
 
-            auto p = map.find(rng());
+        asm volatile("" ::: "memory");
+        doNotOptimize(p);
 
-            asm volatile("" ::: "memory");
-            doNotOptimize(p);
-            if (p != map.end()) {
-                size_t v = p->second;
-                sink += v;
-            }
-            uint64_t t1 = rdtsc_end();
-            stats.observe((double)(t1 - t0));
-        }
+        uint64_t t1 = rdtsc_end();
+        sink += p->second;
+        stats.observe((double)(t1 - t0));
     }
     return stats;
 }
 
 inline Histogram hist_add_uint32_t_google(size_t n) {
+    auto      rngs = random_vec(n);
     Histogram stats;
-
-    size_t       its = 0;
-    std::mt19937 rng(SEED);
+    size_t    its = 0;
 
     while (its < SAMPLES) {
         google::dense_hash_map<uint32_t, size_t> map;
         map.set_empty_key(std::numeric_limits<uint32_t>::max());
 
-        for (size_t i = 0; i < n && its < SAMPLES; i++) {
-            ++its;
+        for (size_t r = 0; r < n && its < SAMPLES; r++, ++its) {
+            auto num = rngs[r];
+
             asm volatile("" ::: "memory");
             uint64_t t0 = rdtsc_start();
 
-            auto& p = map[rng()] += 1;
+            auto& p = map[num] += 1;
 
             asm volatile("" ::: "memory");
             doNotOptimize(p);
 
-            size_t v = p;
-            sink += v;
-
             uint64_t t1 = rdtsc_end();
+            sink += p;
             stats.observe((double)(t1 - t0));
         }
     }
@@ -117,28 +110,26 @@ inline Histogram hist_add_uint32_t_google(size_t n) {
 }
 template <typename Map>
 inline Histogram hist_add_uint32_t(size_t n) {
+    auto      rngs = random_vec(n);
     Histogram stats;
-
-    size_t       its = 0;
-    std::mt19937 rng(SEED);
+    size_t    its = 0;
 
     while (its < SAMPLES) {
         Map map;
 
-        for (size_t i = 0; i < n && its < SAMPLES; i++) {
-            ++its;
+        for (size_t i = 0; i < n && its < SAMPLES; i++, ++its) {
+            auto num = rngs[its];
+
             asm volatile("" ::: "memory");
             uint64_t t0 = rdtsc_start();
 
-            auto& p = map[rng()] += 1;
+            auto& p = map[num] += 1;
 
             asm volatile("" ::: "memory");
             doNotOptimize(p);
 
-            size_t v = p;
-            sink += v;
-
             uint64_t t1 = rdtsc_end();
+            sink += p;
             stats.observe((double)(t1 - t0));
         }
     }
@@ -147,69 +138,56 @@ inline Histogram hist_add_uint32_t(size_t n) {
 
 inline Histogram hist_find_raw_string_view(size_t n) {
     Histogram stats;
-    size_t    its = 0;
+    n = std::min(n, g_corpus.words.size());
 
     RawHashMap<std::string_view, size_t> map;
-    if (n > g_corpus.words.size()) {
-        n = g_corpus.words.size();
-    }
     for (size_t i = 0; i < n; i++) {
         map[g_corpus.words[i]] += 1;
     }
-    while (its < SAMPLES) {
-        for (size_t r = 0; r < n && its < SAMPLES; r++) {
-            ++its;
+    for (size_t its = 0; its != SAMPLES; ++its) {
+        auto word = g_corpus.words[its % n];
 
-            asm volatile("" ::: "memory");
-            uint64_t t0 = rdtsc_start();
+        asm volatile("" ::: "memory");
+        uint64_t t0 = rdtsc_start();
 
-            auto* p = map.get(g_corpus.words[r]);
+        auto* p = map.get(word);
 
-            asm volatile("" ::: "memory");
-            doNotOptimize(p);
-            if (p) {
-                size_t v = *p;
-                sink += v;
-            }
-            uint64_t t1 = rdtsc_end();
-            stats.observe((double)(t1 - t0));
-        }
+        asm volatile("" ::: "memory");
+        doNotOptimize(p);
+
+        uint64_t t1 = rdtsc_end();
+        sink += *p;
+        stats.observe((double)(t1 - t0));
     }
     return stats;
 }
 
 inline Histogram hist_find_google_string_view(size_t n) {
     Histogram stats;
-    size_t    its = 0;
+    n = std::min(n, g_corpus.words.size());
 
     google::dense_hash_map<std::string_view, size_t> map;
     map.set_empty_key(EMPTY_SV);
 
-    if (n > g_corpus.words.size()) {
-        n = g_corpus.words.size();
-    }
     for (size_t i = 0; i < n; i++) {
         map[g_corpus.words[i]] += 1;
     }
 
-    while (its < SAMPLES) {
-        for (size_t r = 0; r < n && its < SAMPLES; r++) {
-            ++its;
+    for (size_t its = 0; its != SAMPLES; ++its) {
+        auto word = g_corpus.words[its % n];
 
-            asm volatile("" ::: "memory");
-            uint64_t t0 = rdtsc_start();
+        asm volatile("" ::: "memory");
+        uint64_t t0 = rdtsc_start();
 
-            auto p = map.find(g_corpus.words[r]);
+        auto p = map.find(word);
 
-            asm volatile("" ::: "memory");
-            doNotOptimize(p);
-            if (p != map.end()) {
-                size_t v = p->second;
-                sink += v;
-            }
-            uint64_t t1 = rdtsc_end();
-            stats.observe((double)(t1 - t0));
-        }
+        asm volatile("" ::: "memory");
+
+        doNotOptimize(p);
+
+        uint64_t t1 = rdtsc_end();
+        sink += p->second;
+        stats.observe((double)(t1 - t0));
     }
     return stats;
 }
@@ -218,22 +196,19 @@ inline Histogram hist_add_string_view_google(size_t n) {
     Histogram stats;
     size_t    its = 0;
 
-    google::dense_hash_map<std::string_view, size_t> map;
-    map.set_empty_key(EMPTY_SV);
-
-    if (n > g_corpus.words.size()) {
-        n = g_corpus.words.size();
-    }
-    for (size_t i = 0; i < n; i++) {}
+    n = std::min(n, g_corpus.words.size());
 
     while (its < SAMPLES) {
-        for (size_t r = 0; r < n && its < SAMPLES; r++) {
-            ++its;
+        google::dense_hash_map<std::string_view, size_t> map;
+        map.set_empty_key(EMPTY_SV);
+
+        for (size_t r = 0; r < n && its < SAMPLES; r++, ++its) {
+            auto word = g_corpus.words[r];
 
             asm volatile("" ::: "memory");
             uint64_t t0 = rdtsc_start();
 
-            auto p = map[g_corpus.words[r]] += 1;
+            auto p = map[word] += 1;
 
             asm volatile("" ::: "memory");
             doNotOptimize(p);
@@ -249,20 +224,17 @@ inline Histogram hist_add_string_view(size_t n) {
     Histogram stats;
     size_t    its = 0;
 
-    Map map;
-    if (n > g_corpus.words.size()) {
-        n = g_corpus.words.size();
-    }
-    for (size_t i = 0; i < n; i++) {}
+    n = std::min(n, g_corpus.words.size());
 
     while (its < SAMPLES) {
-        for (size_t r = 0; r < n && its < SAMPLES; r++) {
-            ++its;
+        Map map;
+        for (size_t r = 0; r < n && its < SAMPLES; r++, ++its) {
+            auto word = g_corpus.words[r];
 
             asm volatile("" ::: "memory");
             uint64_t t0 = rdtsc_start();
 
-            auto p = map[g_corpus.words[r]] += 1;
+            auto p = map[word] += 1;
 
             asm volatile("" ::: "memory");
             doNotOptimize(p);
